2d2131bbf56904919131fd90876a437e9c7fec65
Rawan Tabaja <rawantabaja12@gmail.com>
2022-06-29 15:15:37 +0200

    Tests route POST /avis

--- __test__/avis.spec.js
+++ __test__/avis.spec.js
@@ -26,5 +26,38 @@ describe("avis", () => {
             .get("/avisError")
             .expect(404);
     })
+
+    it("POST /avis", async () => {
+        let insertion = {
+            user: "62bc4d32039bbf0bc32edd45",
+            anime: 2,
+            note: 3,
+            commentaire: "test"
+        }
+
+        const res = await request(app)
+            .post("/avis")
+            .send(insertion)
+            .expect(200)
+            .expect("content-type", /json/);
+        
+        // expect(request(app).get('/avis/'+id)).toMatchObject(insertion);
+    })
+
+    it("POST /avis error: manque le user id dans body", async () => {
+        let insertion = {
+            anime: 2,
+            note: 3,
+            commentaire: "test"
+        }
+
+        const res = await request(app)
+            .post("/avis")
+            .send(insertion)
+            .expect(400)
+            .expect("content-type", /json/);
+        
+        // expect(request(app).get('/avis/'+id)).toMatchObject(insertion);
+    })
     
 });
