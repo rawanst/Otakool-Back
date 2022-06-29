e3f59c16b22710c002bd41558538af33f89765e1
sapey <60222927+sapey@users.noreply.github.com>
2022-06-29 15:17:31 +0200

    Tests route POST /user

--- __test__/user.spec.js
+++ __test__/user.spec.js
@@ -14,10 +14,51 @@ describe("user", () => {
 
     afterAll(() => db.mongoose.connection.close());
 
-    it("GET /user", async () => {
+    it("Route GET /user", async () => {
         const res = await request(app)
             .get("/user")
             .expect(200)
             .expect("content-type", /json/);
     })
+
+    it("Route GET /user error", async () => {
+        const res = await request(app)
+            .get("/userError")
+            .expect(404);
+    })
+
+    it("Route POST /user", async () => {
+        let insertion = {
+            pseudo: "rawan",
+            email: "rawan@gmail.com",
+            password: "rawan",
+            is_moderateur: true
+        }
+
+        const res = await request(app)
+            .post("/user")
+            .send(insertion)
+            .expect(200)
+            .expect("content-type", /json/);
+        
+        // expect(request(app).get('/avis/'+id)).toMatchObject(insertion);
+    })
+
+    it("Route POST /user error : delete password", async () => {
+        let insertion = {
+            pseudo: "rawan",
+            email: "rawan@gmail.com",
+            is_moderateur: true
+        }
+
+        const res = await request(app)
+            .post("/user")
+            .send(insertion)
+            .expect(400)
+            .expect("content-type", /json/);
+        
+        // expect(request(app).get('/avis/'+id)).toMatchObject(insertion);
+    })
+
+
 });
