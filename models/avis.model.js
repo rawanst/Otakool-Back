8e5d02e76a4367c906464110deaff3a31c83e0b1
Rawan Tabaja <rawantabaja12@gmail.com>
2022-06-29 11:07:21 +0200

    Ajout model avis

---
+++ models/avis.model.js
@@ -0,0 +1,18 @@
+module.exports = mongoose => {
+    var schema = mongoose.Schema(
+      {
+        user: Number,
+        anime: Number,
+        note: Number,
+        commentaire: String
+      },
+      { timestamps: true }
+    );
+    schema.method("toJSON", function() {
+      const { __v, _id, ...object } = this.toObject();
+      object.id = _id;
+      return object;
+    });
+    const Avis = mongoose.model("avis", schema);
+    return Avis;
+};
