4f294b7adfff0b579f7a09256190d63f75213d51
Rawan Tabaja <rawantabaja12@gmail.com>
2022-06-29 10:40:22 +0200

    Connection à la BD Mongo + création du model User

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+require("dotenv").config({ path: "./.env" });
 const express = require("express");
 const cors = require("cors");
 const app = express();
@@ -7,6 +8,20 @@ var corsOptions = {
 app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+const db = require("./models");
+db.mongoose
+  .connect(db.url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => {
+    console.log("Connected to the database!");
+  })
+  .catch(err => {
+    console.log("Cannot connect to the database!", err);
+    process.exit();
+  });
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
