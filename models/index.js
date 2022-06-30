f4eeaeab0cc268d6f13a3acaefcc6d699c08f928
sapey <60222927+sapey@users.noreply.github.com>
2022-06-30 14:43:13 +0200

    Peuplement de la base de donée

--- models/index.js
+++ models/index.js
@@ -1,5 +1,6 @@
 const dbConfig = require("../db.config.js");
 const mongoose = require("mongoose");
+const { getMaxListeners } = require("superagent");
 mongoose.Promise = global.Promise;
 const db = {};
 db.mongoose = mongoose;
@@ -8,7 +9,37 @@ db.users = require("./user.model.js")(mongoose);
 db.avis = require("./avis.model.js")(mongoose);
 db.anime = require("./anime.model.js")(mongoose);
 
+// Création de users lors du lancement du projet
+db.users.create({
+    pseudo: "sapey",
+    email: "sapey@gmail.com",
+    password: "sapey",
+    is_moderateur: true
+});
 
+db.users.create({
+    pseudo: "rawan",
+    email: "rawan@gmail.com",
+    password: "rawan",
+    is_moderateur: true
+});
+
+db.users.create({
+    pseudo: "omar",
+    email: "omar@gmail.com",
+    password: "omar",
+    is_moderateur: false
+});
+
+db.users.create({
+    pseudo: "vincent",
+    email: "vincent@gmail.com",
+    password: "vincent",
+    is_moderateur: false
+});
+
+
+// Création d'animé lors du lancement du projet
 db.anime.create({
     type: "anime",
     title: "Cowboy Bebop",
@@ -62,6 +93,8 @@ db.anime.create({
     heightImgMedium: 554,
     episodeCount: 26,
 });
+
+
 
 // db.anime.create({
 //     type: "",
