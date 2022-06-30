1f1292434ebd7752019c542d370a31f542e2109f
Rawan Tabaja <rawantabaja12@gmail.com>
2022-06-30 10:42:55 +0200

    Ajout middleware auth

--- routes/avis.routes.js
+++ routes/avis.routes.js
@@ -1,11 +1,13 @@
 module.exports = app => {
     const avis = require("../controllers/avis.controller.js");
     var router = require("express").Router();
+    const auth = require('../middleware/auth');
+
     // Retrieve all Tutorials
-    router.get("/", avis.findAll);
-    router.get("/:id", avis.findOne);
-    router.post("/", avis.create);
-    router.put("/:id", avis.update);
-    router.delete("/:id", avis.delete);
+    router.get("/", auth, avis.findAll);
+    router.get("/:id", auth, avis.findOne);
+    router.post("/", auth, avis.create);
+    router.put("/:id", auth, avis.update);
+    router.delete("/:id", auth, avis.delete);
     app.use('/avis', router);
   };
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,17 +1,18 @@
 module.exports = app => {
     const user = require("../controllers/user.controller.js");
     var router = require("express").Router();
+    const auth = require('../middleware/auth');
 
     // Retrieve all USER
-    router.get("/", user.findAll);
+    router.get("/", auth, user.findAll);
     // Create a new USER
     router.post("/", user.create);
     // Retrieve a single USER with id
-    router.get("/:id", user.findOne);
+    router.get("/:id", auth, user.findOne);
     // Update a USER with id
-    router.put("/:id", user.update);
+    router.put("/:id", auth, user.update);
     // Delete a USER with id
-    router.delete("/:id", user.delete);
+    router.delete("/:id", auth, user.delete);
     router.post("/login", user.login);
     app.use('/user', router);
   };
