30ade6b0040d0f445df82998f10e7038b367307c
Rawan Tabaja <rawantabaja12@gmail.com>
2022-07-01 14:45:52 +0200

    mise en place cors

--- app.js
+++ app.js
@@ -1,12 +1,9 @@
 const express = require("express");
-// const cors = require("cors");
+const cors = require("cors");
 const app = express();
 const bodyParser = require("body-parser");
 
-// var corsOptions = {
-//   origin: "http://localhost:8081"
-// };
-// app.use(cors(corsOptions));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
